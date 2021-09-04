ef9b794c85b82ec8d733440d249d912a67f6fd36
mansur3 <mansurdewan26@gmail.com>
2021-09-04 20:30:41 +0530

    food

---
+++ food/component/navbar.js
@@ -0,0 +1,15 @@
+function navbar() {
+    return `<div id="navbar">
+    <!--leftside-->
+    <h3><a href="./index.html">Home</a></h3>
+
+    <!--right side-->
+    <div id="options">
+      <h3><a href="./special.html">Latest</a></h3>
+      <h3><a href="./todayReceipe.html">Today Special</a></h3>
+      <h3><a href="">sign up</a></h3>
+      <h3><a href="">Log in</a></h3>
+    </div>
+  </div>`;
+}
+export default navbar;
